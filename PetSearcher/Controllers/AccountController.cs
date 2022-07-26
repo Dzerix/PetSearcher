9b3da1406412c0f9b831be2ac79de1e240384c39
Дмитрий Балабин <dima200223@gmail.com>
2022-07-26 18:06:14 +0300

    Готова страница входаю

---
+++ PetSearcher/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetSearcher.Models;
+
+namespace PetSearcher.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;  
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
